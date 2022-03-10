deb42f5567737a88a08e3c34a2b8315302617283
pferrariog <pdrferrari08@gmail.com>
2022-03-09 21:20:28 -0300

    criando layouts - listagem e detalhe das partidas

---
+++ app/src/main/java/com/simulator/DetailActivity.kt
@@ -0,0 +1,20 @@
+package com.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.simulator.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
---
+++ app/src/main/java/com/simulator/MainActivity.java
@@ -0,0 +1,23 @@
+package com.simulator;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.simulator.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //essa implementação Java corresponde a mesma implementação feita em Kotlin
+    }
+}
